006d3e4b7d3ff88612c4969bd334d2f81b8aa51e
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-25 11:33:07 +0530

    Initial Project without any code

---
+++ BeatBonds-ArtistDataFetch/src/main/java/io/beatbonds/BeatBondsArtistDataFetchApplication.java
@@ -0,0 +1,13 @@
+package io.beatbonds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatBondsArtistDataFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatBondsArtistDataFetchApplication.class, args);
+	}
+
+}
---
+++ BeatBonds-ArtistDataFetch/src/test/java/io/beatbonds/BeatBondsArtistDataFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.beatbonds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatBondsArtistDataFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
