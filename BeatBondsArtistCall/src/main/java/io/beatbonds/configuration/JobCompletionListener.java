11d9b5e8a6c670099166eebbcc8553e836adbd63
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-08-12 14:30:29 +0530

    Created Job Completion Listener and rest template configuration

---
+++ BeatBondsArtistCall/src/main/java/io/beatbonds/configuration/JobCompletionListener.java
@@ -0,0 +1,46 @@
+package io.beatbonds.configuration;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component
+public class JobCompletionListener extends JobExecutionListenerSupport{
+	
+	private static final Logger LOGGER = 
+			LoggerFactory.getLogger(JobCompletionListener.class);
+	
+    private RestTemplate restTemplate;
+	
+    @Autowired
+	public JobCompletionListener(RestTemplate restTemplate) {
+		this.restTemplate=restTemplate;
+	}
+	
+	@Override
+    public void afterJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+        	try {
+        		ResponseEntity<String> responseEntity = restTemplate.postForEntity(
+                        "http://localhost:8181/start-job-b", 
+                        null, 
+                        String.class
+                    );
+            	LOGGER.info(responseEntity.getBody());
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage());
+			}
+        	
+        } else {
+        	LOGGER.error("Something is wrong, job seems not completed.");
+        }
+    }
+}
