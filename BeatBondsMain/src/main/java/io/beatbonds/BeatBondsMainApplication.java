a9413408199bc2fc753a526820ea7d9724489fbf
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-09-20 20:04:13 +0530

    User application

---
+++ BeatBondsMain/src/main/java/io/beatbonds/BeatBondsMainApplication.java
@@ -0,0 +1,13 @@
+package io.beatbonds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatBondsMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatBondsMainApplication.class, args);
+	}
+
+}
